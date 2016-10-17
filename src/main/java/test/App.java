1779c9d938fdea1f8fa7f84b1d3db98beb02571d
romanbortnyk <roman.bortnyk1995@gmail.com>
2016-10-17 14:26:48 +0300

    add mehods : read (String firstName, String lastName ) in UserDao and find (String userType, String disciplineName) in DiscUsrLinkDao

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -1,65 +1,51 @@
 package test;
 
 import dao.implementation.*;
-import model.Avatar;
-import model.Discipline;
-import model.Message;
-import model.User;
+import model.*;
+import persistence.HibernateUtil;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Timestamp;
+import java.util.Iterator;
+import java.util.List;
 
 public class App 
 {
     public static void main( String[] args )
     {
-//        System.out.println("Maven + Hibernate + MySQL");
+
+//        DisciplineDao disciplineDao = new DisciplineDao();
 //
-//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-//        File image = new File(classLoader.getResource("image.png").getFile());
-//        byte[] bFile = new byte[(int) image.length()];
-//        try {
-//            FileInputStream fileInputStream = new FileInputStream(image);
-//            //convert file into array of bytes
-//            fileInputStream.read(bFile);
-//            fileInputStream.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-////
-//        Avatar avatar = new Avatar();
-//        avatar.setImage(bFile);
-
-//        AvatarDao avatarDao = new AvatarDao();
-
-
-//        File hipsta1 = new File("/home/romab/Desktop/hipsta1.png");
-//        File hipsta2 = new File("/home/romab/Desktop/hipsta2.png");
-//        File hipsta3 = new File("/home/romab/Desktop/hipsta3.png");
-//        File hipsta4 = new File("/home/romab/Desktop/hipsta4.png");
+//        File n1 = new File("/home/romab/Desktop/kickboxing.png");
+//        File n2 = new File("/home/romab/Desktop/bodybuilding.png");
 //
-//        avatarDao.createFromFile(hipsta1);
-//        avatarDao.createFromFile(hipsta2);
-//        avatarDao.createFromFile(hipsta3);
-//        avatarDao.createFromFile(hipsta4);
-
-        DisciplineDao disciplineDao = new DisciplineDao();
-//
-
-        File n1 = new File("/home/romab/Desktop/kickboxing.png");
-        File n2 = new File("/home/romab/Desktop/bodybuilding.png");
-
-        disciplineDao.createFromFile("kickboxing", n1);
-        disciplineDao.createFromFile("bodybuilding", n2);
+//        disciplineDao.createFromFile("kickboxing", n1);
+//        disciplineDao.createFromFile("bodybuilding", n2);
 //        disciplineDao.createFromFile("weightlifting", weightlifting);
 //        disciplineDao.createFromFile("swimming", swimming);
 //        disciplineDao.createFromFile("baseball", baseball);
 //        disciplineDao.createFromFile("basketball", basketball);
 
 
+//        DisciplineUserLinkDao disciplineUserLinkDao = new DisciplineUserLinkDao();
+//
+//        List current = disciplineUserLinkDao.find("coach","kickboxing");
+
+        UserDao userDao = new UserDao();
+
+        List current = userDao.read("Vlad", "Gutov");
+
+        Iterator iter = current.iterator();
+        while (iter.hasNext() ){
+            System.out.println(iter.next());
+        }
+
+        HibernateUtil.shutdown();
 
     }
+
+
 }
